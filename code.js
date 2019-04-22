9c2ac41450c555ce03c9c23b614fa4471cdec40d
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 13:26:38 -0700

    Array created and loops

--- code.js
+++ code.js
@@ -1,6 +1,20 @@
-function rot13(str) { 
-   
-    return str;
-  }
-  
-  console.log(rot13("SERR PBQR PNZC"));
+function rot13(str) {
+    let i = 0;
+    let j = 0;
+    let pos = 0;
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    console.log(str.length);
+
+    console.log(alphabet.length);
+    console.log(alphabet.indexOf("F"));
+    while (i < str.length) {
+        console.log(i);
+
+        if (alphabet.indexOf() != -1) {
+            pos = alphabet.indexOf();
+        }
+        i++;
+    }
+}
+
+console.log(rot13("SERR PBQR PNZC"));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+function rot13(str) {
+    let i = 0;
+    // console.log(str.length);
+    while (i < str.length) {
+        console.log(i);
+        i++;
+    }
+}
+
+console.log(rot13("SERR PBQR PNZC"));
+// FREE CODE CAMP
