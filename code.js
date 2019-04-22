b3d3b0e0c48cf590a36b880fdee180de87b6fb49
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 14:03:23 -0700

    array created and some elements added

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ function rot13(str) {
     let j = 0;
     let pos = 0;
     let w;
+    let ca = [];
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     //console.log(str.length);
 
@@ -14,12 +15,14 @@ function rot13(str) {
         if (alphabet.indexOf(w) != -1) {
             pos = alphabet.indexOf(w);
             console.log(pos);
-
+            ca[i] = "L";
         } else {
             console.log("space or number or signal");
+            ca[i] = "O";
         }
         i++;
     }
+    console.log(ca);
 }
 
 console.log(rot13("SERR PBQR! PNZC"));
