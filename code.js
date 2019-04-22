8a41de984d9f17215b7b0beb7545287e491fec71
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 15:44:04 -0700

    some fixes

--- code.js
+++ code.js
@@ -2,20 +2,27 @@ function rot13(str) {
     let i = 0;
     let j = 0;
     let pos = 0;
+    let posm = 0;
+    let posf = 0;
     let w;
     let ca = [];
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    //console.log(str.length);
+    console.log(str);
 
-    //console.log(alphabet.length);
+    console.log("Longitud: " + alphabet.length);
     //console.log(alphabet.indexOf("F"));
     while (i < str.length) {
         // console.log(i);
         w = str.charAt(i);
-        if (alphabet.indexOf(w) != -1) {
+        console.log(str.charAt(i));
+        posm = 0;
+        pos = 0;
+        if (alphabet.indexOf(w) > -1) {
             pos = alphabet.indexOf(w);
-            console.log(pos);
-            ca[i] = "L";
+            posm = pos + 13; //40
+            console.log(pos + " suma: " + pos + "+" + 13 + "=" + posm); //max 26
+            console.log("Sustituye=" + str.charAt(posm));
+            ca.push(str.charAt(posm));
         } else {
             console.log("space or number or signal");
             ca[i] = "O";
