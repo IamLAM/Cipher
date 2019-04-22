48a8fea735a91f36b697c969923822ce70acfa1b
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 13:30:33 -0700

    charAt function added . I detected position of words

--- code.js
+++ code.js
@@ -2,16 +2,19 @@ function rot13(str) {
     let i = 0;
     let j = 0;
     let pos = 0;
+    let w;
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    console.log(str.length);
+    //console.log(str.length);
 
-    console.log(alphabet.length);
-    console.log(alphabet.indexOf("F"));
+    //console.log(alphabet.length);
+    //console.log(alphabet.indexOf("F"));
     while (i < str.length) {
-        console.log(i);
+        // console.log(i);
+        w = str.charAt(i);
+        if (alphabet.indexOf(w) != -1) {
+            pos = alphabet.indexOf(w);
+            console.log(pos);
 
-        if (alphabet.indexOf() != -1) {
-            pos = alphabet.indexOf();
         }
         i++;
     }
