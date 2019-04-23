686c0294cec2723ef5f161d322d0a2b334583437
IamLAM <ingeniero@luismonge.com.mx>
2019-04-22 19:37:34 -0700

    Using charAt and indexOf for searching coincidences and counting words

--- code.js
+++ code.js
@@ -6,30 +6,21 @@ function rot13(str) {
     let posf = 0;
     let w;
     let ca = [];
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    console.log(str);
-
-    console.log("Longitud: " + alphabet.length);
-    //console.log(alphabet.indexOf("F"));
+    alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
     while (i < str.length) {
-        // console.log(i);
         w = str.charAt(i);
-        console.log(str.charAt(i));
-        posm = 0;
-        pos = 0;
-        if (alphabet.indexOf(w) > -1) {
-            pos = alphabet.indexOf(w);
+        if (alpha.indexOf(w) > -1) {
+            pos = alpha.indexOf(w); // return position
             posm = pos + 13; //40
-            console.log(pos + " suma: " + pos + "+" + 13 + "=" + posm); //max 26
-            console.log("Sustituye=" + str.charAt(posm));
-            ca.push(str.charAt(posm));
+            console.log("word:" + w + " Sustituye: " + alpha[posm + 13] + " " + pos + " suma: " + pos + "+" + 13 + "=" + posm); //max 26
+            ca.push(alpha[posm]);
         } else {
             console.log("space or number or signal");
-            ca[i] = "O";
+            ca.push(w);
         }
         i++;
     }
     console.log(ca);
 }
 
-console.log(rot13("SERR PBQR! PNZC"));
+console.log(rot13("SERR PBQR PNZC"));
